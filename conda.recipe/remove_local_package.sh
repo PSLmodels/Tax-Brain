3be8296a36d77c8657cbe1b393ef73d35952144c
andersonfrailey <anderson.frailey@aei.org>
2019-03-07 10:53:25 -0500

    remove taxcalc name

--- conda.recipe/remove_local_package.sh
+++ conda.recipe/remove_local_package.sh
@@ -4,7 +4,7 @@
 # NOTE: for those with experience working with compiled languages,
 #       removing a local conda package is analogous to a "make clean" operation
 
-# uninstall any existing taxcalc conda package
+# uninstall any existing taxbrain conda package
 conda list taxbrain | awk '$1=="taxbrain"{rc=1}END{exit(rc)}'
 if [[ $? -eq 1 ]]; then
     conda uninstall taxbrain --yes 2>&1 > /dev/null
