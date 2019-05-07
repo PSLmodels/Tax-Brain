fdbefe7cf269aa0efaca6d271a7ef6e158b3cb8b
hdoupe <henrymdoupe@gmail.com>
2019-05-07 11:39:25 -0400

    Try with basic python type

--- compconfig/compconfig/functions.py
+++ compconfig/compconfig/functions.py
@@ -52,9 +52,9 @@ def get_inputs(meta_params_dict):
     default_params = {
         "policy": policy_params.specification(
             meta_data=True,
-            start_year=metaparams.year,
+            start_year=metaparams.year.tolist(),
             data_source=metaparams.data_source,
-            use_full_sample=metaparams.use_full_sample,
+            use_full_sample=metaparams.use_full_sample.tolist(),
             serializable=True
         ),
         "behavior": behavior_params.specification(
--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,3 @@
 # bash commands for installing your package
-BUILD_NUM=2
+BUILD_NUM=3
 conda install -c pslmodels taxbrain
