da94b1173dfd9b62afd7c754272619dc5630b4ac
Henry Doupe <henrymdoupe@gmail.com>
2019-05-24 11:34:55 -0400

    Update install.sh

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,3 @@
 # bash commands for installing your package
 BUILD_NUM=1
-conda install -c pslmodels -c conda-forge taxbrain "paramtools>=0.5.4"
+conda install -c pslmodels -c conda-forge nodejs taxbrain "paramtools>=0.5.4"
