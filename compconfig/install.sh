e390bacae72cbdd09befa084181caa2c4cf5f03d
Henry Doupe <henrymdoupe@gmail.com>
2019-08-13 14:15:13 -0400

    Add pandoc to `install.sh` per build error

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,3 @@
 # bash commands for installing your package
 BUILD_NUM=1
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "paramtools>=0.7.0"
+conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "paramtools>=0.7.0" pandoc
