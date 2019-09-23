060b0b6293cb88cbbcf173ab3c94f2b6b5d021b1
Henry Doupe <henrymdoupe@gmail.com>
2019-09-23 17:08:25 -0400

    Fix taxcalc/taxbrain version requirements

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,3 @@
 # bash commands for installing your package
 BUILD_NUM=1
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.5.0" "paramtools>=0.7.0" pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.7.0" pypandoc
