5aeb70db8a70b91ede9c4994b6cf4b3c96c4c520
Peter-Metz <peter.metz@aei.org>
2020-03-24 10:55:32 -0400

    unpin bokeh

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,2 +1,2 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" "bokeh<2.0.0" pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
