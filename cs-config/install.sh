e989e6ce2c75ffbebcc5d0472b4c414d8d5f669e
andersonfrailey <andersonfrailey@gmail.com>
2020-11-21 22:01:56 -0600

    fix merge conflict

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,8 +1,4 @@
 # bash commands for installing your package
 conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
 pip install cs2tc
-<<<<<<< HEAD
 apt-get install texlive-latex-base -y
-=======
-pip install pdflatex
->>>>>>> master
