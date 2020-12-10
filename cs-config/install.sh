74143b4dd7225e0fb612920beef8019e8ba11e46
Henry "Hank" Doupe <henrymdoupe@gmail.com>
2020-12-10 12:32:14 -0500

    Update cs-config deps

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,4 +1,4 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.4.0" "taxcalc>=3.0.0" "paramtools>=0.16.0" bokeh pypandoc
 pip install cs2tc
 apt-get install texlive -y
