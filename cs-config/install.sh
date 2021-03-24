8e5bb28a54b910c060e7217c9bc6dd40afbba905
hdoupe <henrymdoupe@gmail.com>
2021-03-24 10:27:24 -0400

    Simplify `cs-config/install.sh` script for new cs publish logic

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,4 +1,4 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.4.0" "taxcalc>=3.0.0" "paramtools>=0.16.0" bokeh pypandoc
+conda install -c pslmodels -c conda-forge "taxbrain>=2.4.0"
 pip install cs2tc
 apt-get install texlive -y
