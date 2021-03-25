840f8118bc0a45c98171c5d7d411c6c1f4350085
hdoupe <henrymdoupe@gmail.com>
2021-03-25 14:58:07 -0400

    Try no deps

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,3 +1,3 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge "taxbrain>=2.4.0"
+conda install --no-deps -c pslmodels "taxbrain>=2.4.0"
 #apt-get install texlive -y
