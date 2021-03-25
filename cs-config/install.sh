973ca5fd84e53ed9822e222fcc223bc6c3e98ed8
hdoupe <henrymdoupe@gmail.com>
2021-03-25 15:42:42 -0400

    Drop show-current

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,7 +1,6 @@
 # bash commands for installing your package
 git fetch origin tags 2.5.0 && \
   git checkout -b v2.5.0 2.5.0 && \
-  git branch --show-current && \
   pip install --no-deps -e .
 
 apt-get install texlive -y
