890a99c74bb50f582a63d17133051b16b6a20f88
hdoupe <henrymdoupe@gmail.com>
2021-03-25 15:36:17 -0400

    Fetch tag first

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,5 +1,6 @@
 # bash commands for installing your package
-git checkout -b v2.5.0 2.5.0 && \
+git fetch origin tags 2.5.0 && \
+  git checkout -b v2.5.0 2.5.0 && \
   git branch --show-current && \
   pip install --no-deps -e .
 
