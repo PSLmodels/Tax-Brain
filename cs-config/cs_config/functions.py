615047abe736e86e5ea0f4ae17e29a966faaf641
hdoupe <henrymdoupe@gmail.com>
2021-04-09 08:19:52 -0400

    Use default value of None for AWS access keys if not available

--- cs-config/cs_config/functions.py
+++ cs-config/cs_config/functions.py
@@ -16,8 +16,8 @@ from collections import defaultdict, OrderedDict
 from marshmallow import fields
 
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 
