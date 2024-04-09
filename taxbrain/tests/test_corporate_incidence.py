205fd6dcfe2645992d9ecd3692a56fcc7297129d
jdebacker <jason.debacker@gmail.com>
2024-04-08 20:35:47 -0400

    add validation test

--- taxbrain/tests/test_corporate_incidence.py
+++ taxbrain/tests/test_corporate_incidence.py
@@ -1,5 +1,6 @@
 # imports
 import numpy as np
+import pandas as pd
 import taxcalc as tc
 import copy
 import pytest
@@ -264,3 +265,107 @@ def test_share_transition(transition_years, end_year, expected_shares):
 
         # also check that shares always sum to 1
         assert np.allclose(1.0, pct1 + pct2 + pct3)
+
+
+# Validation check
+# Try to use same parameters and recreate TPC
+# (https://www.taxpolicycenter.org/sites/default/files/alfresco/publication-pdfs/412651-How-TPC-Distributes-the-Corporate-Income-Tax.PDF)
+# Table 8, with percent of burden by income quantile
+def test_validation():
+    """
+    Test the transition of share over time in the
+    corp_incidence_distribute function.
+    """
+    # create a calculator object
+    rec = tc.Records.cps_constructor()
+    pol = tc.Policy()
+    calc1 = tc.Calculator(policy=pol, records=rec)
+
+    # set a corp revenue amount
+    revenue = [100_000_000_00.0] * (2034 - 2025)
+
+    # Define different income sources
+    # TODO: should make these constants in corp_incidence.py
+    wage_income_vars = ["e00200"]
+    shareholder_income_vars = ["p22250", "p23250", "e00600", "e00650"]
+    other_capital_income_vars = [
+        "e00300",
+        "e00400",
+        "e01100",
+        "e01200",
+        "e02000",
+    ]
+    # Long run shares
+    param_updates = {
+        "Incidence": {
+            "Labor share": 0.0,
+            "Shareholder share": 1.0,
+            "All capital share": 0.0,
+        },
+        "Long run years": 0,
+    }
+
+    # expected shares if full burden on shareholders:
+    expected = pd.DataFrame(
+        data=[
+            0.000031,
+            0.0,
+            0.001008,
+            0.003991,
+            0.003974,
+            0.008655,
+            0.013243,
+            0.020796,
+            0.027376,
+            0.052368,
+            0.077858,
+            0.790699,
+            1,
+            0.074559,
+            0.122593,
+            0.593546,
+        ],
+        index=[
+            "0-10n",
+            "0-10z",
+            "0-10p",
+            "10-20",
+            "20-30",
+            "30-40",
+            "40-50",
+            "50-60",
+            "60-70",
+            "70-80",
+            "80-90",
+            "90-100",
+            "ALL",
+            "90-95",
+            "95-99",
+            "Top 1%",
+        ],
+    )
+
+    test_calc = corporate_incidence.distribute(
+        copy.deepcopy(calc1), revenue, 2025, 2025, param_updates
+    )
+    calc1.advance_to_year(2025)
+    test_calc.advance_to_year(2025)
+    print("Calc current year = ", calc1.current_year)
+    print("Test Calc current year = ", test_calc.current_year)
+    calc1.calc_all()
+    test_calc.calc_all()
+    dt1, dt2 = calc1.distribution_tables(test_calc, "weighted_deciles")
+    print("Distribution table: ")
+    total_income_change = (
+        dt2.loc["ALL", "expanded_income"] - dt1.loc["ALL", "expanded_income"]
+    )
+    print("total income change = ", total_income_change)
+    pct_change_income = (
+        dt2["expanded_income"] - dt1["expanded_income"]
+    ) / total_income_change
+    print(np.array(pct_change_income.values))
+    print(expected[0].values)
+
+    assert np.allclose(
+        np.array(pct_change_income.values), expected[0].values, atol=1e-5
+    )
